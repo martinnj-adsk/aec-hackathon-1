a8158d3ced249cc2fa844789abb0bff7afbe704e
Martin Johansen <martin.nordby.johansen@autodesk.com>
2024-03-02 15:19:52 +0100

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,68 @@
+import { Forma } from "forma-embedded-view-sdk/auto";
+import { CameraState } from "forma-embedded-view-sdk/camera";
+import { render } from "preact";
+import { useEffect, useState } from "preact/hooks";
+import * as geoData from "./ITU_data.json" assert { type: "json" };
+import { Project } from "forma-embedded-view-sdk/project";
+
+function translateToUtm(
+  geoJson: GeoJSON.FeatureCollection,
+  projString: string
+) {
+  const { features } = geoJson;
+  const projectedFeatures = features.map((feature: any) => {
+    const { geometry, properties } = feature;
+    const { coordinates } = geometry;
+    return {
+      ...geoJson,
+
+      features: projectedFeatures,
+    };
+  });
+}
+
+export default function App() {
+  const [project, setProject] = useState<Project>();
+
+  useEffect(() => {
+    Forma.project.get().then(setProject).catch(console.error);
+  }, []);
+
+  const [currentCamera, setCurrentCamera] = useState<CameraState>();
+  const [initialCamera, setInitialCamera] = useState<CameraState>();
+
+  useEffect(() => {
+    if (!project) return;
+    const;
+  }, [project]);
+
+  useEffect(() => {
+    Forma.camera.getCurrent().then(async (res) => {
+      setCurrentCamera(res);
+      setInitialCamera(res);
+    });
+    Forma.camera.subscribe(setCurrentCamera);
+  }, [setCurrentCamera, setInitialCamera]);
+
+  return (
+    <div>
+      <h2>current Camera position</h2>
+      <div>
+        <pre>{JSON.stringify(currentCamera, null, 2)}</pre>
+      </div>
+      <div>
+        <h2>initial Camera position</h2>
+        <pre>{JSON.stringify(initialCamera, null, 2)}</pre>
+      </div>
+      <button
+        onClick={() =>
+          Forma.camera.move({ ...initialCamera, transitionTimeMs: 1000 })
+        }
+      >
+        Reset to original position
+      </button>
+    </div>
+  );
+}
+
+render(<App />, document.getElementById("app"));
