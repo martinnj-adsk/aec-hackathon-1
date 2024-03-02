eb71645790c5383868fd24dbe36c7563100e26f4
Magnus Poppe Wang <magnus.poppe.wang@autodesk.com>
2024-03-02 17:27:16 +0100

    Display the geojson

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,35 @@
 import { Forma } from "forma-embedded-view-sdk/auto";
 import { CameraState } from "forma-embedded-view-sdk/camera";
 import { render } from "preact";
-import { useEffect, useState } from "preact/hooks";
+import { useEffect, useMemo, useState } from "preact/hooks";
 import { Project } from "forma-embedded-view-sdk/project";
 import { GoogleMaps } from "./GoogleMaps";
+import geojsonRaw from "./ITU_data.json";
+import proj4 from "proj4";
 
-function translateToUtm(
-  geoJson: GeoJSON.FeatureCollection,
-  projString: string
-) {
-  const { features } = geoJson;
-  const projectedFeatures = features.map((feature: any) => {
-    const { geometry, properties } = feature;
-    const { coordinates } = geometry;
-    return {
-      ...geoJson,
-
-      features: projectedFeatures,
-    };
+function translateGeojsonPolygons(
+  geoJson: GeoJSON.FeatureCollection<GeoJSON.Polygon>,
+  project: Project
+): GeoJSON.FeatureCollection<GeoJSON.Polygon> {
+  const wgs84 = "+proj=longlat +datum=WGS84 +no_defs +type=crs";
+  const projectedFeatures = geoJson.features.map((feature: GeoJSON.Feature) => {
+    if (feature.geometry.type === "Polygon") {
+      return {
+        ...feature,
+        geometry: {
+          ...feature.geometry,
+          coordinates: feature.geometry.coordinates.map((c) =>
+            c.map(([x, y]) => {
+              const [xx, yy] = proj4(wgs84, project.projString, [x, y]);
+              return [xx - project.refPoint[0], yy - project.refPoint[1]];
+            })
+          ),
+        },
+      };
+    }
+    throw new Error("Only Polygon features are supported"); // TODO: handle other types
   });
+  return { ...geoJson, features: projectedFeatures };
 }
 
 export default function App() {
@@ -42,6 +53,16 @@ export default function App() {
     });
     Forma.camera.subscribe(setCurrentCamera);
   }, [setCurrentCamera, setInitialCamera]);
+
+  useEffect(() => {
+    if (!project) return;
+    const geojson = translateGeojsonPolygons(
+      geojsonRaw as GeoJSON.FeatureCollection<GeoJSON.Polygon>,
+      project
+    );
+    console.log(geojson);
+    Forma.render.geojson.add({ geojson });
+  }, [project]);
 
   return (
     <div>
